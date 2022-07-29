5b7c235e825659b775b2e348892ad58eabd0a309
TheAwiteb <awiteb@hotmail.com>
2022-07-29 13:25:28 +0300

    ✨: Impl `Default` for `Config` struct

--- src/config/config_utils.rs
+++ src/config/config_utils.rs
@@ -16,79 +16,109 @@
 //     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 use crate::errors::{ApcError, ApcResult};
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use std::fs;
 use std::path::Path;
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Serialize, Debug, Educe)]
+#[educe(Default)]
 /// Select action configuration structure
 pub struct SelectAction {
+    #[educe(Default = "What do you want to do ❓")]
     /// The question of select action
     pub select_action_message: String,
+    #[educe(Default = "Create a new post ✍")]
     /// Create a new post choice in select
     pub new_post_choice: String,
+    #[educe(Default = "Update modified date 📅")]
     /// Update modified post date choice
     pub update_modified_post_date: String,
+    #[educe(Default = "Alepc Version ⚙")]
     /// Version choice
     pub version_choice: String,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Serialize, Debug, Educe)]
+#[educe(Default)]
 /// Inputs setting structure
 pub struct InputSettings {
+    #[educe(Default = 7)]
     /// Minimum length of post title
     pub minimum_title_length: u8,
+    #[educe(Default = 30)]
     /// Maximum length of post title
     pub maximum_title_length: u8,
+    #[educe(Default = "Title of post 📝")]
     /// Ask for post title message
     pub title_message: String,
 
+    #[educe(Default = 10)]
     /// Minimum length of post description
     pub minimum_description_length: u8,
+    #[educe(Default = 255)]
     /// Maximum length of post description
     pub maximum_description_length: u8,
+    #[educe(Default = "Description of post 📝")]
     /// Ask for post description message
     pub description_message: String,
 
+    #[educe(Default = 1)]
     /// Minimum tags on post
     pub minimum_tags_count: u8,
+    #[educe(Default = 3)]
     /// Maximum tags on post
     pub maximum_tags_count: u8,
+    #[educe(Default = "Tags of post (separated by comma)")]
     /// Ask for post tags message
     pub tags_message: String,
+    #[educe(Default = ',')]
     /// separated tags by
     pub separated_tags_by: char,
+    #[educe(Default = 3)]
     /// Minimum single tag length
     pub minimum_single_tag_length: u8,
+    #[educe(Default = 8)]
     /// Maximum single tag length
     pub maximum_single_tag_length: u8,
 
+    #[educe(Default = "Slug of post")]
     /// Ask for post slug message
     pub slug_message: String,
+    #[educe(Default = 5)]
     /// Minimum length of post slug
     pub minimum_slug_length: u8,
+    #[educe(Default = 20)]
     /// Maximum length of post slug
     pub maximum_slug_length: u8,
 
+    #[educe(Default = "Image of post")]
     /// Ask for post image message
     pub image_message: String,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Serialize, Debug, Educe)]
+#[educe(Default)]
 /// Config structure for Alepc
 pub struct Config {
+    #[educe(Default = "../Aleecers.github.io/src/pages/blog/")]
     /// Path of posts
     pub posts_path: String,
+    #[educe(Default = "../Aleecers.github.io/public/images/")]
     /// Path to images directory
     pub images_path: String,
+    #[educe(Default = "/blog/")]
     /// Path of blog in the site
     pub blog_site_path: String,
+    #[educe(Default = "/images/")]
     /// Path of images in the site
     pub images_site_path: String,
+    #[educe(Default = "../../layouts/blog.astro")]
     /// Layout path of posts ( path start from `posts_path` )
     pub posts_layout: String,
+    #[educe(Default = "https://github.com/aleecers/alepc")]
     /// Repository url
     pub repository_url: String,
+    #[educe(Default = "%Y/%m/%d")]
     /// Date format
     pub date_format: String,
     /// Select action structure
