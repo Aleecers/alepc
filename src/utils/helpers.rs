096115d7262ad8cdb9bab66e9944c71a0086a299
TheAwiteb <awiteb@hotmail.com>
2022-08-17 20:04:24 +0300

    ✨: Add helper module

---
+++ src/utils/helpers.rs
@@ -0,0 +1,26 @@
+// Simple CLI to create aleecers post template
+//     Copyright (C) 2020-2022  TheAwiteb
+//     https://github.com/aleecers/Alepc
+//
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+use crate::config::Config;
+use requestty::Answers;
+
+pub fn new_post(config: &'static Config) -> impl Fn(&Answers) -> bool {
+    move |answers| {
+        answers.get("action").unwrap().as_list_item().unwrap().text
+            == config.select_action.new_post_choice
+    }
+}
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -16,6 +16,7 @@
 //     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 mod creator;
+pub mod helpers;
 pub mod questions;
 pub mod validators;
 
